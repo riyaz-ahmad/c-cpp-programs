9055f996afa434a090c1c3382d9592f947b5b439
riyaz-ahmad <ahmadjahangir610@gmail.com>
2021-09-21 23:30:54 +0530

    Interesting c++ codes

---
+++ Pattern2.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n =5, m=5;
+    for(int i=n; i>=1;i--){
+        for(int j=1;j<=i;j++){
+           /* if(j<=i)
+                cout<<"*";
+            else   
+                cout<<" ";*/
+                 cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
